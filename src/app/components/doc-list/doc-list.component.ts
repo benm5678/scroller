df277d49a65b958c057f70148f5a0f9ab15c45b4
Ben <benm@Bens-MacBook-Pro.local>
2020-12-02 13:38:53 -0700

    initial formatting

--- src/app/components/doc-list/doc-list.component.ts
+++ src/app/components/doc-list/doc-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DocManagerService } from 'src/app/services/doc-manager.service';
 
 @Component({
   selector: 'app-doc-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private docItemService: DocManagerService) { }
 
   ngOnInit(): void {
   }
